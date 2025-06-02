f4c3e1cd1f6bd57795788bec68323ee75e380c14
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-06-02 19:32:54 +0200

    Adding image to label

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import tkinter as tk
-from PIL import Image
+from PIL import Image, ImageTk
 
 
+#region CONSTANTS
+image = Image.open(r'./algebra_ucionica.jpg')
+#endregion
+
+
+
+
+
+root = tk.Tk()
+root.title('Algebra - Py Photoshop')
+
+lbl_photo_image = ImageTk.PhotoImage(image=image)
+lbl_photo = tk.Label(root, text='FOTOGRAFIJA', image=lbl_photo_image)
+lbl_photo.grid(column=0, row=0)
+
+
+if __name__ == '__main__':
+    root.mainloop()
